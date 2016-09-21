0ae51e4ab54a0de542d9ab7e7bbc88d15131e3ae
Nigel Nelson <nigel.mark.nelson@gmail.com>
2016-09-21 05:19:33 +0000

    Moving module.exports up to the top
    ... since it doesn't matter when I set module.exports. Cleaner
    that way, in my opinion, although maybe I will see some convention
    later which changes my mind.

--- server/api.js
+++ server/api.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var bodyParser = require('body-parser');
-var crypto = require('crypto');
+module.exports = router;
 
+var bodyParser = require('body-parser');
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
+
+var crypto = require('crypto');
 
 const PAGE_SIZE = 20;
 const REFRESH_MILLIS = 2000;
@@ -141,4 +143,3 @@ router.use((err, req, res, next) => {
 });
 
 // export
-module.exports = router;
--- server/serve-frontend.js
+++ server/serve-frontend.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
+module.exports = router;
 
 router.get('/', (req, res) => res.sendFile(`${__dirname}/html/home.html`));
 router.get('/rp/:url', (req, res) => res.sendFile(`${__dirname}/html/rp.html`));
-
-module.exports = router;
