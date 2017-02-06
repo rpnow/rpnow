28b22535b31c80fdfa6bb81f28c3b0baf0a330c3
Nigel Nelson <nigel.mark.nelson@gmail.com>
2017-02-06 00:59:02 -0700

    changed 'var' to 'const'

--- server/serve-frontend.js
+++ server/serve-frontend.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 module.exports = router;
 
 router.get('/', (req, res) => res.sendFile(`${__dirname}/html/home.html`));
