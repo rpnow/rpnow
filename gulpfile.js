4de802fadcf7622e977e2acb243cd7afd1448286
Nigel Nelson <nigel.mark.nelson@gmail.com>
2017-03-23 06:42:29 -0600

    gulp watches changes for development purposes

--- gulpfile.js
+++ gulpfile.js
@@ -7,17 +7,20 @@ gulp.task('app.js', () =>
         .pipe(babel({ presets: ['env'] }))
         .pipe(gulp.dest('build/app'))
 )
+gulp.watch('src/www/app/app.js', ['app.js']);
 
 gulp.task('html', () =>
     gulp.src('src/www/app/*.html')
         .pipe(gulp.dest('build/app'))
 )
+gulp.watch('src/www/app/*.html', ['html']);
 
 gulp.task('css', () =>
     gulp.src('src/www/app/*.css')
         .pipe(cssc())
         .pipe(gulp.dest('build/app'))
 )
+gulp.watch('src/www/app/*.css', ['css']);
 
 gulp.task('app', [ 'app.js', 'html', 'css' ])
 
