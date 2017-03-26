e5ea583de37e07dd5660d580721787af01c59cc5
Nigel Nelson <nigel.mark.nelson@gmail.com>
2017-03-26 16:32:36 -0600

    refactoring; using console.error when we shouldnt
    Also not attaching api stopping event listeners until the api has started

--- server/index.js
+++ server/index.js
@@ -2,12 +2,17 @@ const api = require('./api');
 api.trustProxy = true;
 
 api.start((err) => {
-    console.error(err || 'RPNow API: ready.');
-});
+    if (err) return console.error(err);
 
-process.on('SIGTERM', ()=> shutdown('SIGTERM') ); //kill (terminate)
-process.on('SIGINT', ()=> shutdown('SIGINT') ); //Ctrl+C (interrupt)
-function shutdown(reason) {
-    console.log(`RPNow API: Attempting graceful shutdown: ${reason}`);
-    api.stop((err)=> console.log(err || 'RPNow API: Shutdown complete.') );
-}
+    console.log('RPNow API: ready.');
+
+    process.on('SIGTERM', ()=> shutdown('SIGTERM') ); //kill (terminate)
+    process.on('SIGINT', ()=> shutdown('SIGINT') ); //Ctrl+C (interrupt)
+    function shutdown(reason) {
+        console.log(`RPNow API: Attempting graceful shutdown: ${reason}`);
+        api.stop((err)=> {
+            if (err) console.error(err);
+            else console.log('RPNow API: Shutdown complete.');
+        });
+    }
+});
