c05991059601ec0ed0a5d5b4a5d0e6f9a04ae014
Nigel Nelson <nigel.mark.nelson@gmail.com>
2017-08-04 19:53:22 -0600

    put the image controller code inside of rp-feed
    also, rp-feed now just uses its parent's entire rp object rather than asking to resolve just the msgs (we need the whole thing now)

--- client/app/rp-view/rp-feed.component.js
+++ client/app/rp-view/rp-feed.component.js
@@ -6,10 +6,7 @@ angular.module('rpnow')
     $stateProvider.state({
         name: 'rp.feed',
         url: '',
-        component: 'rpFeed',
-        resolve: {
-            msgs: ['rp', rp => rp.msgs]
-        }
+        component: 'rpFeed'
     })
 }])
 
@@ -18,12 +15,12 @@ angular.module('rpnow')
     template: ``,
     controller: 'RpFeedController',
     bindings: {
-        msgs: '<',
+        rp: '<',
         showDetails: '<'
     }
 })
 
-.controller('RpFeedController', [function() {
+.controller('RpFeedController', ['$mdDialog', function($mdDialog) {
     const $ctrl = this;
 
     var RECENT_MSG_COUNT = 100;
@@ -33,4 +30,47 @@ angular.module('rpnow')
         $ctrl.isStoryGlued = true;
         $ctrl.numMsgsToShow = RECENT_MSG_COUNT;
     };
+
+    $ctrl.showImageDialog = function(evt) {
+        return $mdDialog.show({
+            controller: 'ImageDialogController',
+            controllerAs: '$ctrl',
+            locals: { rp: $ctrl.rp },
+            bindToController: true,
+            templateUrl: '/rp-view/image-dialog.template.html',
+            parent: angular.element(document.body),
+            targetEvent: evt,
+            clickOutsideToClose: true
+        });
+    }
+}])
+
+.controller('ImageDialogController', ['$mdDialog', '$mdToast', function($mdDialog, $mdToast) {
+    this.hide = () => $mdDialog.cancel();
+    this.url = '';
+    this.sending = false;
+
+    //https://github.com/angular/angular.js/blob/master/src/ngSanitize/filter/linky.js#L3
+    var urlRegexp = /^((ftp|https?):\/\/|(www\.)?[A-Za-z0-9._%+-]+@)\S*[^\s.;,(){}<>"\u201d\u2019]$/gi
+    this.isValid = () => this.url.match(urlRegexp);
+    
+    this.send = () => {
+        this.rp.addImage(this.url, (err) => {
+            this.sending = false;
+            if (err) {
+                var errMsg;
+                if (err.code === 'URL_FAILED')
+                    errMsg = `Couldn't reach the site at: ${this.url}`
+                else if (err.code === 'BAD_CONTENT')
+                    errMsg = `The content at "${this.url}" does not appear to be an image. Check the URL and try again.`;
+                else
+                    errMsg = `An unknown error occurred! Server says: "${err.code}"`
+
+                $mdToast.show($mdToast.simple().textContent(errMsg));
+                return;
+            }
+            $mdDialog.hide();
+        });
+        this.sending = true;
+    }
 }])
