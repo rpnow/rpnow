a8334557b3c36d5fd862a181e781837309883274
Nigel Nelson <nigel.mark.nelson@gmail.com>
2017-12-27 18:12:29 -0700

    created some test RP's in my local environment, putting the URL's in the repo for convenience

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,8 @@ import { Component } from '@angular/core';
     <h1>Angular Router</h1>
     <nav>
       <a routerLink="/">Home</a>
-      <a routerLink="/rp/abcdefgh">RP 1</a>
-      <a routerLink="/rp/12345678">RP 2</a>
+      <a routerLink="/rp/dj95iZmY">RP 1</a>
+      <a routerLink="/rp/buDmYX4s">RP 2</a>
     </nav>
     <h2>Page below</h2>
     <router-outlet></router-outlet>
