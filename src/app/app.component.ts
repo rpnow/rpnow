4ce85baa5bc6a5e8db66d015d46847b728c31dd9
Nigel Nelson <nigel.mark.nelson@gmail.com>
2017-12-30 20:22:06 -0700

    a couple other test RP's, and then one that definitely doesn't exist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,9 @@ import { Component } from '@angular/core';
       <a routerLink="/">Home</a>
       <a routerLink="/rp/dj95iZmY">RP 1</a>
       <a routerLink="/rp/buDmYX4s">RP 2</a>
+      <a routerLink="/rp/HoDWoSoB">RP 3</a>
+      <a routerLink="/rp/aGpsvC3g">RP 4</a>
+      <a routerLink="/rp/00000000">RP bad</a>
     </nav>
     <h2>Page below</h2>
     <router-outlet></router-outlet>
