f8c6ec81a908ea3e34935d8eb8410f299124d243
Nigel Nelson <nigel.mark.nelson@gmail.com>
2018-01-19 11:04:59 -0700

    WIP message-box

--- src/app/rp/chat/message-box/message-box.component.ts
+++ src/app/rp/chat/message-box/message-box.component.ts
@@ -2,20 +2,18 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'rp-message-box',
-  template: `
-    <textarea [(ngModel)]="content"></textarea>
-    <button (click)="sendMessage()">Send</button>
-  `,
-  styles: []
+  templateUrl: 'message-box.html',
+  styleUrls: ['message-box.scss']
 })
 export class MessageBoxComponent {
 
   public content: '';
+  public type = 'narrator';
 
   @Output() onMessage: EventEmitter<{content: string, type: string, charaId?: number}> = new EventEmitter();
 
   sendMessage() {
-    this.onMessage.emit({content: this.content, type: 'narrator'});
+    this.onMessage.emit({content: this.content, type: this.type});
     this.content = '';
   }
 }
