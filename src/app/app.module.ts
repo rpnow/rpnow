e662bb394a116473a39252a0b66c70b9d81324df
Nigel Nelson <nigel.mark.nelson@gmail.com>
2018-01-28 16:38:15 -0700

    autoresize for message box textarea

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -56,6 +57,7 @@ import { NewCharaComponent } from './rp/chat/new-chara/new-chara.component';
     MatDialogModule,
     MatDividerModule,
     MatIconModule,
+    MatInputModule,
     MatListModule,
     MatProgressSpinnerModule,
     MatSidenavModule,
