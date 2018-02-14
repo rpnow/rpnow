e6c5799e0f5f9e00c3f40186ebcc3857282a9eef
Nigel Nelson <nigel.mark.nelson@gmail.com>
2018-02-14 16:47:53 -0700

    make all options available as subjects

--- src/app/options.service.ts
+++ src/app/options.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Challenge } from './challenge.service';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 // export interface RoomsOptions {
 // 	[key: string]: {
@@ -9,77 +10,48 @@ import { Challenge } from './challenge.service';
 // 	}
 // }
 
+
+function subject<T>(propName: string, defaultValue: T): BehaviorSubject<T> {
+	let localStorageKey = 'rpnow.global.'+propName;
+	let stringValue = localStorage.getItem(localStorageKey);
+	let value = (stringValue != null) ? JSON.parse(stringValue) : defaultValue;
+
+	let subj = new BehaviorSubject(value);
+
+	subj.subscribe(value => localStorage.setItem(localStorageKey, JSON.stringify(value)));
+
+	return subj;
+}
+
 @Injectable()
 export class OptionsService {
 
-	/*
-	 * Functions to manipulate and access localstorage
-	 */
-	private cache: any = {};
+	public readonly lastColor$: BehaviorSubject<string> = subject('lastColor', '#80c9ff');
+	public get lastColor() { return this.lastColor$.value; }
+	public set lastColor(value) { this.lastColor$.next(value); }
 
-	private get(propName: string, defaultValue: any) {
-		if (!(propName in this.cache)) {
-			let value = localStorage.getItem('rpnow.global.'+propName);
-			this.cache[propName] = (value != null) ? JSON.parse(value) : defaultValue;
-		} 
-		return this.cache[propName];
-	}
+	public readonly challenge$: BehaviorSubject<Readonly<Challenge>> = subject('challenge', null);
+	public get challenge() { return this.challenge$.value; }
+	public set challenge(value) { this.challenge$.next(value); }
 
-	private set(propName: string, value: any) {
-		this.cache[propName] = JSON.parse(JSON.stringify(value));
-		localStorage.setItem('rpnow.global.'+propName, JSON.stringify(value));
-	}
+	public readonly downloadOOC$: BehaviorSubject<boolean> = subject('downloadOOC', true);
+	public get downloadOOC() { return this.downloadOOC$.value; }
+	public set downloadOOC(value) { this.downloadOOC$.next(value); }
 
-	/*
-	 * Global options definitions
-	 */
-	public get lastColor(): string {
-		return this.get('lastColor', '#80c9ff')
-	}
-	public set lastColor(value: string) {
-		this.set('lastColor', value)
-	}
+	public readonly nightMode$: BehaviorSubject<boolean> = subject('nightMode', false);
+	public get nightMode() { return this.nightMode$.value; }
+	public set nightMode(value) { this.nightMode$.next(value); }
 
-	public get challenge(): Readonly<Challenge> {
-		return this.get('challenge', null)
-	}
-	public set challenge(value: Readonly<Challenge>) {
-		this.set('challenge', value)
-	}
+	public readonly notificationNoise$: BehaviorSubject<number> = subject('notificationNoise', 1);
+	public get notificationNoise() { return this.notificationNoise$.value; }
+	public set notificationNoise(value) { this.notificationNoise$.next(value); }
 
-	public get downloadOOC(): boolean {
-		return this.get('downloadOOC', true)
-	}
-	public set downloadOOC(value: boolean) {
-		this.set('downloadOOC', value)
-	}
+	public readonly pressEnterToSend$: BehaviorSubject<boolean> = subject('pressEnterToSend', true);
+	public get pressEnterToSend() { return this.pressEnterToSend$.value; }
+	public set pressEnterToSend(value) { this.pressEnterToSend$.next(value); }
 
-	public get nightMode(): boolean {
-		return this.get('nightMode', false)
-	}
-	public set nightMode(value: boolean) {
-		this.set('nightMode', value)
-	}
-
-	public get notificationNoise(): number {
-		return this.get('notificationNoise', 1)
-	}
-	public set notificationNoise(value: number) {
-		this.set('notificationNoise', value)
-	}
-
-	public get pressEnterToSend(): boolean {
-		return this.get('pressEnterToSend', true)
-	}
-	public set pressEnterToSend(value: boolean) {
-		this.set('pressEnterToSend', value)
-	}
-
-	public get showMessageDetails(): boolean {
-		return this.get('showMessageDetails', true)
-	}
-	public set showMessageDetails(value: boolean) {
-		this.set('showMessageDetails', value)
-	}
+	public readonly showMessageDetails$: BehaviorSubject<boolean> = subject('showMessageDetails', true);
+	public get showMessageDetails() { return this.showMessageDetails$.value; }
+	public set showMessageDetails(value) { this.showMessageDetails$.next(value); }
 
 }
