4d4b1a82ad312af042f4a431722e9d1b84d6020d
Nigel Nelson <nigel.mark.nelson@gmail.com>
2018-03-17 10:05:06 -0600

    Smoother, better loading experience

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd, NavigationCancel } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  template: `<router-outlet></router-outlet>`,
-  styles: []
+  template: `
+    <div *ngIf="loading" id="loading">
+      <p>Loading RPNow layout...</p>
+      <mat-spinner></mat-spinner>
+    </div>
+    <router-outlet *ngIf="!loading"></router-outlet>
+  `,
+  styles: [`
+    #loading {
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+      align-items: center;
+      justify-content: center;
+    }
+  `]
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  constructor(private router: Router) {}
+  loading = false;
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(e => e instanceof NavigationStart)
+    ).subscribe(() => this.loading = true)
+    this.router.events.pipe(
+      filter(e => (e instanceof NavigationEnd) || (e instanceof NavigationCancel))
+    ).subscribe(() => this.loading = false)
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
@@ -17,6 +18,7 @@ import { TitleModule } from './title/title.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatProgressSpinnerModule,
     ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production}),
     TitleModule,
     AppRoutingModule
