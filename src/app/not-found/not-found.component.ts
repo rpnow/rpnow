531473c69e1be13e09b7706479c85c85ee44ea46
Nigel Nelson <nigel.mark.nelson@gmail.com>
2018-03-19 13:52:49 -0600

    404 page

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,19 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-not-found',
   template: `
-    <p>
-      not-found works!
-    </p>
+    <div id="not-found">
+      <h1>Page not found: <code>{{route}}</code></h1>
+
+      <p>Sorry, there's no page at this address. Make sure you've spelled the URL correctly, or <a routerLink="/">return to the RPNow homepage.</a></p>
+      
+      <p>If you believe this is an error, contact <a href="mailto:rpnow.net@gmail.com">rpnow.net@gmail.com</a>.</p>
+    </div>
   `,
-  styles: []
+  styles: [`
+    #not-found {
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+      align-items: center center;
+      justify-content: center;
+      margin: auto;
+      max-width: 400px;
+      padding: 10px;
+    }
+  `]
 })
 export class NotFoundComponent implements OnInit {
 
-  constructor() { }
+  route = location.pathname;
+
+  constructor(private titleService: Title) { }
 
   ngOnInit() {
+    this.titleService.setTitle('Page Not Found | RPNow')
   }
 
 }
