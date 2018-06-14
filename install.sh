9b61ecc99dccbbba1491418a6d03bfa4ae1747b3
Nigel Nelson <nigel.mark.nelson@gmail.com>
2018-06-14 00:39:26 +0000

    update docker config for node 8

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-docker-compose run --rm api npm cache clean
 docker-compose run --rm api npm install
 
-docker-compose run --rm web_build npm cache clean
 docker-compose run --rm web_build npm install --unsafe-perm
