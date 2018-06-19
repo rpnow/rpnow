7549ed91cb4703d7c17889724f44cdc19f80db38
Nigel Nelson <nigel.mark.nelson@gmail.com>
2018-06-19 06:32:51 +0000

    compress static + SPA routes in nodejs

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ const logger = require('./services/logger');
 const config = require('./config');
 const express = require('express');
 const { createServer } = require('http');
+const compression = require('compression');
 
 logger.debug('Starting RPNow API...');
 
@@ -15,10 +16,13 @@ app.use('/api', require('./api/api.rest'));
 require('./api/api.sockets')(server);
 
 // static file serving + SPA routes
+const staticRoutes = new express.Router();
 const staticDir = __dirname.replace('server/src', 'client/dist');
-app.use(express.static(staticDir));
-app.get('/terms', (req, res) => res.sendFile(`${staticDir}/index.html`));
-app.get('/rp/*', (req, res) => res.sendFile(`${staticDir}/index.html`));
+staticRoutes.use(compression());
+staticRoutes.use(express.static(staticDir));
+staticRoutes.get('/terms', (req, res) => res.sendFile(`${staticDir}/index.html`));
+staticRoutes.get('/rp/*', (req, res) => res.sendFile(`${staticDir}/index.html`));
+app.use(staticRoutes);
 
 // listen
 server.listen(config.get('port'), (err) => {
