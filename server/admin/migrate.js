5a1ed404b222940516e8e76a2b0747fe20553985
Nigel Nelson <nigel.mark.nelson@gmail.com>
2018-06-26 02:35:41 +0000

    finished migration script

--- server/admin/migrate.js
+++ server/admin/migrate.js
@@ -10,24 +10,34 @@ async function run() {
 
     let roomsInProgress = 0;
     rooms.forEach(async (room) => {
-        console.log(`Room ${room.rpCode} IN > ${room.msgs.length} ${room.charas.length}`);
+        console.log(`Room ${room.rpCode} ---- msgs:${room.msgs.length}   charas:${room.charas.length}`);
 
         ++roomsInProgress;
 
-        if (room.msgs) {
-            await db.collection('messages').deleteMany({ roomId: room._id });
-
-            if (room.msgs.length > 0) await db.collection('messages').insertMany(room.msgs.map(m => ({ ...m, roomId: room._id })));
-
-            await db.collection('rooms').updateOne({ _id: room._id }, { $unset: { msgs: '' } });
-        }
+        let newCharaIds = null;
 
         if (room.charas) {
             await db.collection('charas').deleteMany({ roomId: room._id });
 
-            if (room.charas.length > 0) await db.collection('charas').insertMany(room.charas.map(c => ({ ...c, roomId: room._id })));
+            if (room.charas.length > 0) {
+                newCharaIds = (await db.collection('charas').insertMany(room.charas.map(c => ({ ...c, roomId: room._id })))).insertedIds;
+            }
 
             await db.collection('rooms').updateOne({ _id: room._id }, { $unset: { charas: '' } });
+        }
+
+        if (room.msgs) {
+            await db.collection('messages').deleteMany({ roomId: room._id });
+
+            if (room.msgs.length > 0) {
+                await db.collection('messages').insertMany(room.msgs.map(m => (
+                    (m.charaId != null) ?
+                        { ...m, roomId: room._id, charaId: newCharaIds[m.charaId] } :
+                        { ...m, roomId: room._id }
+                )));
+            }
+
+            await db.collection('rooms').updateOne({ _id: room._id }, { $unset: { msgs: '' } });
         }
 
         if (room.rpCode) {
