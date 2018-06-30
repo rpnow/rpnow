cad068998684bee5590286fb79909bbf93f08c66
Nigel Nelson <nigel.mark.nelson@gmail.com>
2018-06-30 08:02:08 +0000

    discard custom route reuse strategy in favor of onSameUrlNavigation

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes, PreloadAllModules, RouteReuseStrategy, DetachedRouteHandle, ActivatedRouteSnapshot } from '@angular/router';
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 
 import { NotFoundComponent } from './not-found/not-found.component';
 
@@ -18,46 +18,14 @@ const appRoutes: Routes = [
   }
 ];
 
-export class CustomReuseStrategy implements RouteReuseStrategy {
-  storedRouteHandles = new Map<string, DetachedRouteHandle>();
-
-  // Decides if the route should be stored
-  shouldDetach(route: ActivatedRouteSnapshot): boolean {
-    return false;
-  }
-
-  // Store the information for the route we're destructing
-  store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
-    this.storedRouteHandles.set(route.routeConfig.path, handle);
-  }
-
-  // Return true if we have a stored route object for the next route
-  shouldAttach(route: ActivatedRouteSnapshot): boolean {
-    return false;
-  }
-
-  // If we returned true in shouldAttach(), now return the actual route data for restoration
-  retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle {
-    return this.storedRouteHandles.get(route.routeConfig.path);
-  }
-
-  // Reuse the route if we're going to and from the same route
-  shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
-    return future.routeConfig === curr.routeConfig &&
-      JSON.stringify(future.params) === JSON.stringify(curr.params);
-  }
-}
-
 @NgModule({
   imports: [
     RouterModule.forRoot(appRoutes, {
       paramsInheritanceStrategy: 'always',
-      preloadingStrategy: PreloadAllModules
+      preloadingStrategy: PreloadAllModules,
+      onSameUrlNavigation: 'reload'
     })
   ],
-  exports: [RouterModule],
-  providers: [
-    { provide: RouteReuseStrategy, useClass: CustomReuseStrategy }
-  ]
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
