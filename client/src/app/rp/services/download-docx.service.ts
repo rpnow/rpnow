d06a8830b00a1ef63619d05615f99d2af9fd62e3
Nigel Nelson <nigel.mark.nelson@gmail.com>
2018-06-30 21:23:13 +0000

    use the server API for downloading now, rather than generating it on the client

--- client/src/app/rp/services/download-docx.service.ts
+++ client/src/app/rp/services/download-docx.service.ts
@@ -1,54 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { RpService } from './rp.service';
-import * as Docxtemplater from 'docxtemplater';
-import * as JSZip from 'jszip';
-import { saveAs } from 'file-saver';
 import { TrackService } from '../../track.service';
+import { RpCodeService } from './rp-code.service';
+import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class DownloadDocxService {
 
   constructor(
-    private http: HttpClient,
-    private rp: RpService,
+    private rpCodeService: RpCodeService,
     private track: TrackService
   ) { }
 
-  private docxTemplateRequest = this.http.get('/client-files/assets/template.docx', {responseType: 'blob'}).toPromise();
-
   public downloadDocx(includeOOC: boolean) {
-    this.track.event('Download', 'docx', includeOOC ? 'ooc: yes' : 'ooc: no', this.rp.messages.length);
-
-    const data = {
-      title: this.rp.title,
-      desc: this.rp.desc,
-      hasDesc: !!this.rp.desc,
-      msgs: this.rp.messages.map(({type, content, url, charaId}) => ({
-        content,
-        url,
-        isNarrator: (type === 'narrator'),
-        isOOC: (type === 'ooc'),
-        isImage: (type === 'image'),
-        isChara: (type === 'chara'),
-        name: (type === 'chara' ? this.rp.charasById.get(charaId).name.toUpperCase() : undefined)
-      })).filter(msg => includeOOC || !msg.isOOC)
-    };
-
-    this.docxTemplateRequest.then(blob => {
-      const reader = new FileReader();
-      reader.onload = () => {
-        const doc = new Docxtemplater().loadZip(new JSZip(reader.result));
-        doc.setData(data);
-        doc.render();
-        const file = doc.getZip().generate({
-          type: 'blob',
-          mimeType: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-        });
-        saveAs(file, this.rp.title + '.docx');
-      };
-      reader.readAsArrayBuffer(blob);
-    });
+    this.track.event('Download', 'docx', includeOOC ? 'ooc: yes' : 'ooc: no');
+    const url = `${environment.apiUrl}/api/rp/${this.rpCodeService.rpCode}/download.docx${includeOOC ? '?includeOOC=true' : ''}`;
+    window.open(url, '_blank');
   }
 
 }
--- client/src/app/rp/services/download-txt.service.ts
+++ client/src/app/rp/services/download-txt.service.ts
@@ -1,60 +1,20 @@
 import { Injectable } from '@angular/core';
-import * as wrap from 'word-wrap';
-import { saveAs } from 'file-saver';
-import { RpService } from './rp.service';
 import { TrackService } from '../../track.service';
+import { RpCodeService } from './rp-code.service';
+import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class DownloadTxtService {
 
   constructor(
-    private rp: RpService,
+    private rpCodeService: RpCodeService,
     private track: TrackService
   ) { }
 
   public downloadTxt(includeOOC: boolean) {
-    this.track.event('Download', 'txt', includeOOC ? 'ooc: yes' : 'ooc: no', this.rp.messages.length);
-
-    // get rp formatted text
-    const text = this.rpText(this.rp, includeOOC);
-
-    // save as file
-    const blob = new Blob([text], { type: 'text/plain;charset=utf-8'});
-    saveAs(blob, `${this.rp.title}.txt`);
+    this.track.event('Download', 'txt', includeOOC ? 'ooc: yes' : 'ooc: no');
+    const url = `${environment.apiUrl}/api/rp/${this.rpCodeService.rpCode}/download.txt${includeOOC ? '?includeOOC=true' : ''}`;
+    window.open(url, '_blank');
   }
 
-  private rpText(rp: RpService, includeOOC: boolean): string {
-    // get messages
-    let msgs = rp.messages;
-
-    // ignore ooc messages if so desired
-    if (!includeOOC) msgs = msgs.filter(msg => msg.type !== 'ooc');
-
-    // header format
-    const header = `${rp.title}\n${rp.desc || ''}\n----------`;
-
-    // word-wrap and format all messages
-    const body = msgs.map(msg => {
-      if (msg.type === 'narrator') {
-        return wrap(msg.content, { width: 72, indent: '', cut: true });
-      } else if (msg.type === 'ooc') {
-        return wrap(`(( OOC: ${msg.content} ))`, { width: 72, indent: '', cut: true });
-      } else if (msg.type === 'chara') {
-        return rp.charasById.get(msg.charaId).name.toUpperCase() + ':\n' +
-          wrap(msg.content, { width: 70, indent: '  ', cut: true });
-      } else if (msg.type === 'image') {
-        return `--- IMAGE ---\n${msg.url}\n-------------`;
-      } else {
-        throw new Error(`Unexpected message type: ${msg.type}`);
-      }
-    });
-
-    // contents of the text file
-    const fileContents = [ header, ...body ]
-      .join('\n\n') // double newlines separate each piece
-      .replace(/\n/g, '\r\n'); // format it for windows
-
-    // done
-    return fileContents;
-  }
 }
