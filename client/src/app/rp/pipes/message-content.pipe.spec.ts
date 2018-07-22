6b9c1ed112a933cfb12db1e5699407d639fc8dda
Nigel <nigel.mark.nelson@gmail.com>
2018-07-21 23:15:37 -0600

    improve message-content pipe tests

--- client/src/app/rp/pipes/message-content.pipe.spec.ts
+++ client/src/app/rp/pipes/message-content.pipe.spec.ts
@@ -3,29 +3,39 @@ import { MessageContentPipe } from './message-content.pipe';
 import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
 import { SecurityContext } from '@angular/core';
 
-describe('MessageContentPipe', () => {
+const tests: { [testName: string]: [string, string|null, string][] } = {
 
-  const tests: [string, [string, string|null, string][]][] = [
-    ['regular strings', [
-      ['Hello world!', null, 'Hello world!'],
-      ['', null, ''],
-    ]],
-    ['/italics/', [
-      ['/Test/', null, '<i>Test</i>'],
-      ['Time to /Test/ this', null, 'Time to <i>Test</i> this']
-    ]],
-    ['_italics_', [
-      ['_Test_', null, '<i>Test</i>'],
-      ['Time to _Test_ this', null, 'Time to <i>Test</i> this'],
-    ]],
-    ['__bold__', [
-      ['__Test__', null, '<b>Test</b>'],
-      ['Time to __Test__ this', null, 'Time to <b>Test</b> this'],
-    ]],
-    ['newlines', [
-      ['Two\nlines', null, 'Two<br>lines']
-    ]]
-  ];
+  'regular strings': [
+    ['Hello world!', null, 'Hello world!'],
+    ['', null, ''],
+  ],
+
+  'escape': [
+    ['<b>Escape this</b>', null, '&lt;b&gt;Escape this&lt;/b&gt;'],
+  ],
+
+  '/italics/': [
+    ['/Test/', null, '<i>Test</i>'],
+    ['Time to /Test/ this', null, 'Time to <i>Test</i> this'],
+  ],
+
+  '_italics_': [
+    ['_Test_', null, '<i>Test</i>'],
+    ['Time to _Test_ this', null, 'Time to <i>Test</i> this'],
+  ],
+
+  '__bold__': [
+    ['__Test__', null, '<b>Test</b>'],
+    ['Time to __Test__ this', null, 'Time to <b>Test</b> this'],
+  ],
+
+  'newlines': [
+    ['Two\nlines', null, 'Two<br>lines'],
+  ],
+
+};
+
+describe('MessageContentPipe', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -33,7 +43,7 @@ describe('MessageContentPipe', () => {
     });
   });
 
-  for (const [itText, pairs] of tests) {
+  for (const [itText, pairs] of Object.entries(tests)) {
     it(itText, inject([DomSanitizer], (domSanitizer: DomSanitizer) => {
       const pipe = new MessageContentPipe(domSanitizer);
       for (const [input, color, expected] of pairs) {
