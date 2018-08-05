72cab2ad7cec7e6e9fa0ed349454c54f5803ec63
Nigel <nigel.mark.nelson@gmail.com>
2018-08-04 22:50:00 -0600

    homepage logo size/positioning tweaks

--- client/src/app/title/components/lets-rpnow.component.ts
+++ client/src/app/title/components/lets-rpnow.component.ts
@@ -10,17 +10,17 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   `,
   styles: [`
     aside {
-      transform: translate(calc(-25vmin - 16px)) rotate(-10deg);
+      transform: translate(calc(-20vmin - 32px)) rotate(-10deg);
       text-align: center;
       margin-bottom: 0;
-      font-size: calc(16px + 8vmin);
+      font-size: calc(16px + 7vmin);
       line-height: 1;
       font-family: "Alice";
     }
     h1 {
       margin: -2vmin 0 0;
       text-align: center;
-      font-size: calc(24px + 16vmin);
+      font-size: calc(24px + 14vmin);
       line-height: 1;
       font-family: "Playfair Display";
       font-weight: normal;
