a75603c2025ab9d86bb59e1d08ba3eb9d8c461b5
Nigel Nelson <nigel.mark.nelson@gmail.com>
2018-11-04 15:08:37 -0700

    Basic Slim3 routes for Apache/PHP app

---
+++ php-server/index.php
@@ -0,0 +1,15 @@
+<?php
+require 'vendor/autoload.php';
+// Create and configure Slim app
+$config = ['settings' => [
+    'addContentLengthHeader' => false,
+]];
+$app = new \Slim\App($config);
+
+// Define app routes
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    return $response->write("Hello " . $args['name']);
+});
+
+// Run app
+$app->run();
