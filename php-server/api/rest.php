38d71849f14af662e2dfd3a2bf340bea33ef435c
Nigel Nelson <nigel.mark.nelson@gmail.com>
2018-11-06 20:33:30 -0700

    Stubbing out REST API methods

--- php-server/api/rest.php
+++ php-server/api/rest.php
@@ -1,8 +1,33 @@
 <?php
 
 $app->group('/api', function() {
-    $this->get('/{name}', function ($request, $response, $args) {
-        return $response->write("Hello " . $args['name']);
-        return $response->withStatus(200);
+    $this->post('/rp.json', function ($request, $response, $args) {
+        return $response->withJson(['foo' => 'bar'], 200);
+    });
+    $this->post('/challenge.json', function ($request, $response, $args) {
+        return $response->withJson(['foo' => 'bar'], 200);
+    });
+    $this->group('/rp/{rpCode:[-0-9a-zA-Z]+}', function() {
+        $this->post('/message', function ($request, $response, $args) {
+            return $response->withJson(['rpCode' => $args['rpCode']], 200);
+        });
+        $this->post('/image', function ($request, $response, $args) {
+            return $response->withJson(['rpCode' => $args['rpCode']], 200);
+        });
+        $this->post('/chara', function ($request, $response, $args) {
+            return $response->withJson(['rpCode' => $args['rpCode']], 200);
+        });
+        $this->patch('/message', function ($request, $response, $args) {
+            return $response->withJson(['rpCode' => $args['rpCode']], 200);
+        });
+        $this->patch('/chara', function ($request, $response, $args) {
+            return $response->withJson(['rpCode' => $args['rpCode']], 200);
+        });
+        $this->get('/page/{pageNum:[1-9][0-9]*}', function ($request, $response, $args) {
+            return $response->withJson(['rpCode' => $args['rpCode']], 200);
+        });
+        $this->get('/download.txt', function ($request, $response, $args) {
+            return $response->withJson(['rpCode' => $args['rpCode']], 200);
+        });
     });
 });
