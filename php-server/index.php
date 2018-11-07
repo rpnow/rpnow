c6527c1d7f43345bc52146e0e4bd23560ad2c44f
Nigel Nelson <nigel.mark.nelson@gmail.com>
2018-11-07 01:17:09 -0700

    quick hack for CORS for testing

--- php-server/index.php
+++ php-server/index.php
@@ -7,6 +7,16 @@ $config = ['settings' => [
 ]];
 $app = new \Slim\App($config);
 
+// CORS
+// TODO remove this for prod
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', 'http://localhost:4200')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
 // Dependencies
 require_once './api/db.php';
 
