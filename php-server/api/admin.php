036aa6d2ee981bcca9b49a43f05d9e3efbfc6212
Nigel Nelson <nigel.mark.nelson@gmail.com>
2018-11-09 14:37:51 -0700

    WIP, the guts of the document store is coming along well

--- php-server/api/admin.php
+++ php-server/api/admin.php
@@ -2,23 +2,7 @@
 
 $app->group('/admin', function() {
     $this->get('/setup', function ($request, $response, $args) {
-        $this->get('pdo')->exec(
-            "CREATE TABLE docs (
-                event_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                namespace TEXT NOT NULL,
-                doc_id TEXT NOT NULL,
-                revision_age INT DEFAULT 0 NOT NULL,
-                body TEXT,
-                timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
-                ip TEXT,
-                hash TEXT,
-
-                CONSTRAINT doc_id_namespace_table UNIQUE (namespace, revision_age, doc_id)
-            )"
-        );
-        $this->get('pdo')->exec(
-            "CREATE INDEX hash_index ON docs (hash) WHERE hash IS NOT NULL"
-        );
+        $this->get('docs')->initializeDb();
         return $response->withJson(['ok'], 200);
     });
 });
