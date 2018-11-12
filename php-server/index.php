b54c02e35d15248df077e2b6ecf080c0ab1a064d
Nigel Nelson <nigel.mark.nelson@gmail.com>
2018-11-12 01:11:03 -0700

    Serve frontend from PHP

--- php-server/index.php
+++ php-server/index.php
@@ -30,5 +30,23 @@ require_once './api/admin.php';
 require_once './api/rest.php';
 require_once './api/stream.php';
 
+// Frontend
+// TODO X-Robots-Tag
+$app->get('/', function ($request, $response, $args) {
+    return $response->write(file_get_contents('./static/index.html'));
+});
+$app->get('/terms', function ($request, $response, $args) {
+    return $response->write(file_get_contents('./static/index.html'));
+});
+$app->get('/rp/demo', function ($request, $response, $args) {
+    return $response->write(file_get_contents('./static/index.html'));
+});
+$app->get('/rp/{rpRoute:.*}', function ($request, $response, $args) {
+    return $response->write(file_get_contents('./static/index.html'));
+});
+$app->get('{badRoute:.*}', function ($request, $response, $args) {
+    return $response->withStatus(404)->write(file_get_contents('./static/index.html'));
+});
+
 // Run app
 $app->run();
