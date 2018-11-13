7919c4ef9e0850243763336b90587178020ab6cc
Nigel Nelson <nigel.mark.nelson@gmail.com>
2018-11-13 14:13:02 -0700

    Revert "quick hack for CORS for testing"
    This reverts commit c6527c1d7f43345bc52146e0e4bd23560ad2c44f.

--- php-server/index.php
+++ php-server/index.php
@@ -12,16 +12,6 @@ $config = ['settings' => [
 ]];
 $app = new \Slim\App($config);
 
-// CORS
-// TODO remove this for prod
-$app->add(function ($req, $res, $next) {
-    $response = $next($req, $res);
-    return $response
-            ->withHeader('Access-Control-Allow-Origin', 'http://localhost:4200')
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-});
-
 // Dependencies
 require_once './api/db.php';
 
