d3f269792130d5bcb806e0b5d83157f2bfb38027
Nigel Nelson <nigel.mark.nelson@gmail.com>
2018-11-19 11:51:49 -0700

    Refactor frontedn-routes and db-settings out of index.php

---
+++ php-server/db-settings.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'driver'    => 'mysql',
+    'prefix'    => '',
+    'host'      => 'db',
+    'database'  => 'rpnow',
+    'username'  => 'nigel',
+    'password'  => 'abc123'
+];
--- php-server/index.php
+++ php-server/index.php
@@ -1,46 +1,29 @@
 <?php
+// Load libraries
 require 'vendor/autoload.php';
+
 // Create and configure Slim app
-$config = ['settings' => [
-    'addContentLengthHeader' => false,
-    'displayErrorDetails' => true,
-    'db' => [
-        'driver' => 'mysql',
-        'prefix' => '',
-        'host' => 'db',
-        'database' => 'rpnow',
-        'username' => 'nigel',
-        'password' => 'abc123'
+$config = [
+    'settings' => [
+        // TODO add this back if we end up using stream api
+        // 'addContentLengthHeader' => false,
+
+        'displayErrorDetails' => true,
+
+        'db' => require './db-settings.php'
     ]
-]];
+];
 $app = new \Slim\App($config);
 
-// Dependencies
+// Dependency injection
 require_once './src/db.php';
-require_once './src/stream.php';
 require_once './src/validate.php';
-require_once './src/pubsub.php';
 
-// Define app routes
+// App API
 require_once './src/rest.php';
 
 // Frontend
-// TODO X-Robots-Tag
-$app->get('/', function ($request, $response, $args) {
-    return $response->write(file_get_contents('./client-files/index.html'));
-});
-$app->get('/terms', function ($request, $response, $args) {
-    return $response->write(file_get_contents('./client-files/index.html'));
-});
-$app->get('/rp/demo', function ($request, $response, $args) {
-    return $response->write(file_get_contents('./client-files/index.html'));
-});
-$app->get('/rp/{rpRoute:.*}', function ($request, $response, $args) {
-    return $response->write(file_get_contents('./client-files/index.html'));
-});
-$app->get('{badRoute:.*}', function ($request, $response, $args) {
-    return $response->withStatus(404)->write(file_get_contents('./client-files/index.html'));
-});
+require_once './src/frontend-routes.php';
 
 // Run app
 $app->run();
