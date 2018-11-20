7c5909f414bb336cfdf221d304f54984939e3702
Nigel Nelson <nigel.mark.nelson@gmail.com>
2018-11-20 15:07:36 -0700

    Remove some old stuff the mini server won't use

--- server/src/events/sub.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-ROOM=${1:-abc}
-FILE=./fifo_room_${ROOM}_$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32)
-
-mkfifo $FILE
-trap "rm -f $FILE" 0
-trap "exit 1" HUP INT QUIT PIPE TERM
-tail -f $FILE
