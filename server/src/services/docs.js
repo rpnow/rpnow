b8312c863580ef84ac0ad63c63275de9596186df
Nigel Nelson <nigel.mark.nelson@gmail.com>
2018-11-26 17:39:50 -0700

    Docs service refactoring

--- server/src/services/docs.js
+++ server/src/services/docs.js
@@ -16,11 +16,10 @@ const connected = (async function connect() {
             t.string('_id', 64).notNullable();
             t.integer('revision').notNullable();
             t.json('body', 50000).nullable();
-            t.timestamp('timestamp').defaultTo(knex.fn.now()).notNullable();
+            t.timestamp('timestamp').notNullable();
             t.string('ip', 47).nullable();
             t.string('auth_hash').nullable();
 
-            t.unique(['namespace', 'collection', 'revision', '_id']);
             t.unique(['namespace', 'collection', '_id', 'revision']);
             t.index('timestamp');
             t.index('ip');
@@ -30,67 +29,49 @@ const connected = (async function connect() {
 }());
 
 const formatQueryResult = (x) => {
-    const { body, ...meta } = x;
-    return { ...JSON.parse(body), ...meta };
+    const { event_id, namespace, collection, body, auth_hash, ...meta } = x;
+    const deleted = (body == null);
+    return { ...JSON.parse(body), ...meta, deleted };
 };
 
 const Docs = {
     async create(namespace, collection, _id, body, ip) {
-        // TODO return entire created doc
-        return knex('docs').insert({ namespace, collection, _id, ip, revision: 0, body: JSON.stringify(body) });
+        await connected;
+        const timestamp = new Date().toISOString();
+        const revision = 0;
+        const doc = { namespace, collection, _id, ip, revision, timestamp, body: JSON.stringify(body) };
+        const [eventId] = await knex('docs').insert(doc);
+        return { eventId, doc: formatQueryResult(doc) };
     },
 
     async update(namespace, collection, _id, body, ip) {
+        await connected;
         if(!(await this.exists(namespace, collection, _id))) {
             throw new Error(`Document ${collection}:${_id} does not exist`);
         }
-        return this.put(namespace, collection, _id, body, ip);
-    },
-
-    async put(namespace, collection, _id, body, ip) {
-        await connected;
         // TODO just get lastRevision inside of the query
-        const lastRevision = (await knex('docs').where({ namespace, collection, _id }).max('revision AS value').first()).value;
-        await knex('docs').insert({ namespace, collection, _id, ip, revision: lastRevision+1, body: JSON.stringify(body) });
-        // TODO return entire updated doc
-    },
-
-    async exists(namespace, collection, _id) {
-        return (await this.doc(namespace, collection, _id)) != null;
-    },
-
-    async doc(namespace, collection, _id, filters = {}) {
-        let q = knex('docs')
-            .where('docs.namespace', namespace)
-            .where('docs.collection', collection)
-            .where('docs._id', _id)
-
-        if (filters.snapshot != null) {
-            q = q.where('docs.event_id', '<=', filters.snapshot);
-        }
-        if (!filters.includeHistory) {
-            q = q.leftJoin('docs as newerDocs', function() {
-                    this.on('docs.namespace', 'newerDocs.namespace');
-                    this.andOn('docs.collection', 'newerDocs.collection');
-                    this.andOn('docs._id', 'newerDocs._id');
-                    this.andOn('docs.event_id', '<', 'newerDocs.event_id');
-                    if (filters.snapshot != null) {
-                        this.andOn('newerDocs.event_id', '<=', knex.raw('?', [filters.snapshot]));
-                    }
-                })
-                .whereNull('newerDocs._id')
-                .select('docs.*');
-        }
-
-        const result = await q.first();
-        return formatQueryResult(result);
+        const [{ lastRevision }] = await knex('docs').where({ namespace, collection, _id }).max('revision AS lastRevision');
+        const revision = lastRevision + 1;
+        const timestamp = new Date().toISOString();
+        const doc = { namespace, collection, _id, ip, revision, timestamp, body: JSON.stringify(body) };
+        const [eventId] = await knex('docs').insert(doc);
+        return { eventId, doc: formatQueryResult(doc) };
     },
 
     docs(namespace, collection, filters) {
+        // shortcut for getting just _id
+        if (typeof filters === 'string') {
+            const _id = filters;
+            return this.docs(namespace, collection, { _id });
+        }
+
         let q = knex('docs').where('docs.namespace', namespace)
 
         if (collection != null) {
             q = q.where('docs.collection', collection);
+        }
+        if (filters._id != null) {
+            q = q.where('docs._id', filters._id);
         }
         if (filters.since != null) {
             q = q.where('docs.event_id', '>', filters.since);
@@ -125,6 +106,11 @@ const Docs = {
             .orderBy('docs.revision', 'asc')
         
         return {
+            async single() {
+                await connected;
+                const result = await q.first();
+                return formatQueryResult(result);
+            },
             async asArray() {
                 await connected;
                 const res = await q;
@@ -146,8 +132,18 @@ const Docs = {
         };
     },
 
+    async doc(...args) {
+        return this.docs(...args).single();
+    },
+
+    async exists(namespace, collection, _id) {
+        return (await this.doc(namespace, collection, _id)) != null;
+    },
+
     async lastEventId() {
-        return (await knex('docs').max('event_id AS value').first()).value;
+        await connected;
+        const [{ value }] = await knex('docs').max('event_id AS value');
+        return value;
     },
 };
 
