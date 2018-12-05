03252ecbf157718f976a616e2c027e8e74f692db
Nigel Nelson <nigel.mark.nelson@gmail.com>
2018-12-05 03:20:14 -0700

    Load a test rp-message.vue component through httpVueLoader

---
+++ src/web/rp-message.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <input v-model="content">
+    {{ content }}
+  </div>
+</template>
+  
+<script>
+  module.exports = {
+    props: ['content'],
+    data: function() {
+      return {
+      }
+    }
+  }
+</script>
+  
