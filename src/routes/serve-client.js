fbef24dfe3912ec882e50da5e08872cbb5e9c4c0
Nigel Nelson <nigel.mark.nelson@gmail.com>
2018-12-12 17:37:09 -0700

    Add terms page

--- src/routes/serve-client.js
+++ src/routes/serve-client.js
@@ -9,7 +9,7 @@ const clientFiles = path.join(__dirname, '../web');
 staticRoutes.use('/client-files/', express.static(clientFiles));
 // valid routes
 staticRoutes.get('/', (req, res) => res.sendFile(`${clientFiles}/home.html`));
-staticRoutes.get('/terms', (req, res) => res.sendFile(`${clientFiles}/index.html`));
+staticRoutes.get('/terms', (req, res) => res.sendFile(`${clientFiles}/terms.txt`));
 staticRoutes.get('/rp/demo', (req, res) => res.sendFile(`${clientFiles}/index.html`)); // separate, because no xRobotsTag
 staticRoutes.get('/rp/[^/]+', xRobotsTag, (req, res) => res.sendFile(`${clientFiles}/rp.html`));
 staticRoutes.get('/read/[^/]+/page/[0-9]+', (req, res) => res.sendFile(`${clientFiles}/rp-read.html`));
