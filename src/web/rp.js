50f1ccaa6a56d5461b52147c7416d0b34f55d0d7
Nigel Nelson <nigel.mark.nelson@gmail.com>
2018-12-28 18:47:38 -0700

    Remove commented-out noises, which are no longer used

--- src/web/rp.js
+++ src/web/rp.js
@@ -25,17 +25,6 @@ var jsonStorage = (function() {
   }
 })();
 
-// var audioDir = '/client-files/assets/sounds/';
-// var noises = [
-//   {'name': 'Off', 'audio': null},
-//   {'name': 'Typewriter', 'audio': new Audio(audioDir + 'typewriter.mp3')},
-//   {'name': 'Page turn', 'audio': new Audio(audioDir + 'pageturn.mp3')},
-//   {'name': 'Chimes', 'audio': new Audio(audioDir + 'chimes.mp3')},
-//   {'name': 'Woosh', 'audio': new Audio(audioDir + 'woosh.mp3')},
-//   {'name': 'Frog block', 'audio': new Audio(audioDir + 'frogblock.mp3')},
-//   {'name': 'Classic alert', 'audio': new Audio(audioDir + 'alert.mp3')},
-// ];
-
 /**
  * Tries to determine if this is a desktop keyboard by measuring
  * the average length of time between keydown and keyup events on
