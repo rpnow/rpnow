ce95d14601a6af4dc379058d72587798162601ec
Nigel Nelson <nigel.mark.nelson@gmail.com>
2018-12-30 00:43:18 -0700

    Extract vue html head data to its own file

--- src/routes/serve-client.js
+++ src/routes/serve-client.js
@@ -2,6 +2,7 @@ const express = require('express');
 const expressVue = require('express-vue');
 const path = require('path');
 const { xRobotsTag } = require('../services/express-x-robots-tag-middleware');
+const vueHead = require('../views/head');
 
 const router = new express.Router();
 
@@ -11,43 +12,7 @@ router.use('/client-files/', express.static(clientFiles));
 
 // .vue file rendering
 const viewFiles = path.join(__dirname, '../views');
-router.use(expressVue.init({
-    rootPath: viewFiles,
-    head: {
-        metas: [
-            { charset:'utf-8' },
-
-            { name:"viewport", content:"width=device-width, initial-scale=1, maximum-scale=1" },
-            { name:"apple-mobile-web-app-capable", content:"yes" },
-            { name:"apple-mobile-web-app-status-bar-style", content:"default" },
-            { name:"theme-color", content:"#fafafa" },
-
-            { rel: "icon", type: "image/png", href: "/client-files/assets/favicon/favicon-16x16.png", sizes: "16x16" },
-            { rel:"icon", type:"image/png", href:"/client-files/assets/favicon/favicon-32x32.png", sizes:"32x32" },
-            { rel:"icon", type:"image/png", href:"/client-files/assets/favicon/favicon-96x96.png", sizes:"96x96" },
-            { rel:"apple-touch-icon", href:"/client-files/assets/favicon/favicon-128x128.png" },
-            { rel:"stylesheet", href:"https://fonts.googleapis.com/css?family=Alice|Playfair+Display" },
-            { rel:"stylesheet", href:"https://fonts.googleapis.com/icon?family=Material+Icons" },
-            { rel:"manifest", href:"/client-files/manifest.json" },
-        ],
-        scripts: [
-            // Promise polyfill for older browsers, needed for axios
-            { src: "https://cdn.jsdelivr.net/npm/es6-promise@4.2.5/dist/es6-promise.auto.min.js" },
-            // Axios, our http library
-            { src: "https://cdn.jsdelivr.net/npm/axios@0.18.0/dist/axios.min.js" },
-            // coolstory.js, which gives us fun random titles for stories
-            { src: "https://cdn.jsdelivr.net/npm/coolstory.js@0.1.2/coolstory.js" },
-            // jQuery is required for Spectrum, but please do not use it for other things
-            { src: "https://cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.min.js" },
-            // Spectrum is a colorpicker. It also happens to include the tinycolor library
-            { src: "https://cdn.jsdelivr.net/gh/bgrins/spectrum@1.8.0/spectrum.js" },
-        ],
-        styles: [
-            // Spectrum.js styles
-            { style: "https://cdn.jsdelivr.net/gh/bgrins/spectrum@1.8.0/spectrum.css" },
-        ]
-    }
-}));
+router.use(expressVue.init({ rootPath: viewFiles, head: vueHead }));
 
 // valid routes
 router.get('/', (req, res) => res.renderVue('home.vue', null, { head: { title: 'RPNow' } }));
