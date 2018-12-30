bff6b0155e314ff2c7c226bbc6fd51e8c7fba020
Nigel Nelson <nigel.mark.nelson@gmail.com>
2018-12-30 01:11:43 -0700

    Remove old angular client

--- old-angular-client/src/app/version.ts
+++
@@ -1,2 +0,0 @@
-export const version = '2.1.9';
-console.log(`RPNow version ${version}`);
--- old-angular-client/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  assetRoot: '/client-files/',
-  apiUrl: '',
-  wsUrl: `${location.protocol === 'https:' ? 'wss' : 'ws'}://${location.host}`,
-};
