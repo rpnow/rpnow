40aefb7b60b5f5bc350d20542f5d56807167c9de
Nigel Nelson <nigel.mark.nelson@gmail.com>
2018-12-31 13:24:37 -0700

    WIP build with nexe. The problem is that express-vue doesn't seem to work

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Clean dist folder
-rm -rf dist
-mkdir dist
+rm -rf dist/*
+mkdir -p dist
 
 # Get native sqlite3 bindings for all platforms, and copy them in
 npx node-pre-gyp install --directory ./node_modules/sqlite3 --target_platform=win32 --target_arch=ia32
@@ -21,14 +21,15 @@ rm -rf node_modules/ursa-optional
 cp ./rpnow.ini ./dist
 
 # Build exe files
-npx pkg ./package.json --targets node8-win-x86 --output dist/RPNow.exe
-npx pkg ./package.json --targets node8-linux-x64 --output dist/rpnow
+npx nexe src/index.js -t linux-x64 -r src/views -r node_modules/sqlite3 -r node_modules/knex -r node_modules/vue-pronto -o dist/rpnow-linux-x64
 
 # Zip up the distributables
-cd dist
-zip rpnow-windows.zip RPNow.exe rpnow.ini node_modules/sqlite3/lib/binding/node-v57-win32-ia32/node_sqlite3.node
-zip rpnow-linux.zip rpnow rpnow.ini node_modules/sqlite3/lib/binding/node-v57-linux-x64/node_sqlite3.node
+# cd dist
+# zip -r rpnow-win-x64.zip rpnow-win-x64.exe static views node_modules/sqlite3/lib/binding/node-v57-win32-x64/node_sqlite3.node
+# zip -r rpnow-win-x86.zip rpnow-win-x86.exe static views node_modules/sqlite3/lib/binding/node-v57-win32-ia32/node_sqlite3.node
+# zip -r rpnow-macos-x64.zip rpnow-macos-x64 static views node_modules/sqlite3/lib/binding/node-v57-darwin-x64/node_sqlite3.node
+# zip -r rpnow-linux-x64.zip rpnow-linux-x64 static views node_modules/sqlite3/lib/binding/node-v57-linux-x64/node_sqlite3.node
 
 # Clean up dist folder
-rm -rf node_modules
-find . -type f ! -name '*.zip' -delete
+# rm -rf node_modules src
+# find . -type f ! -name '*.zip' -delete
