1a3c0033a22132c9e92b6e9ab8cbd801e3759f81
Nigel Nelson <nigel.mark.nelson@gmail.com>
2019-01-16 14:10:14 -0700

    Better fix for charas with null timestamps

--- server/src/services/download.json.js
+++ server/src/services/download.json.js
@@ -14,15 +14,30 @@ module.exports = ({
 
         charas = charas.map(({ timestamp, name, color }) => ({ timestamp: timestamp ? new Date(timestamp*1000).toISOString(): null, name, color }))
 
-        rpStream.write(`[\n${JSON.stringify({ title, desc, charas })}`);
+        let writtenMeta = false;
 
         msgStream.on('data', (msg) => {
             const { timestamp, type, content, url, charaId } = msg;
             msg = ({ timestamp: new Date(timestamp*1000).toISOString(), type, content, url, charaId: charaIdMap.get(charaId && charaId.toString())});
+
+            if (!writtenMeta) {
+                // sometimes charas don't have timestamps so we just steal from the first message
+                charas = charas.map(({timestamp, ...stuff}) => ({ timestamp: timestamp || msg.timestamp, ...stuff }))
+                rpStream.write(`[\n${JSON.stringify({ title, desc, charas })}`);
+                writtenMeta = true;
+            }
+
             rpStream.write(`,\n${JSON.stringify(msg)}`)
         });
 
         msgStream.on('end', () => {
+            if (!writtenMeta) {
+                // sometimes charas don't have timestamps so we just steal from the first message
+                charas = charas.map(({timestamp, ...stuff}) => ({ timestamp: timestamp || msg.timestamp, ...stuff }))
+                rpStream.write(`[\n${JSON.stringify({ title, desc, charas })}`);
+                writtenMeta = true;
+            }
+
             rpStream.write('\n]\n');
             rpStream.end()
         });
