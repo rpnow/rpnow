51d0caa7cc92b2f0ec898431635ebed34d814924
Nigel Nelson <nigel.mark.nelson@gmail.com>
2019-01-17 17:29:13 -0700

    Refactor banner message out of index, & check if already running

---
+++ src/services/cli-banner-message.js
@@ -0,0 +1,39 @@
+const os = require('os');
+const config = require('./config');
+
+const myIpAddresses = Object.values(os.networkInterfaces())
+    .map(x => x.find(({ family, internal }) => family === 'IPv4' && !internal))
+    .filter(x => x)
+    .map(x => x.address);
+
+const banner = `
+ _ __ _ __  _ __   _____      __
+| '__| '_ \\| '_ \\ / _ \\ \\ /\\ / /
+| |  | |_) | | | | (_) \\ V  V /
+|_|  | .__/|_| |_|\\___/ \\_/\\_/
+     |_|         pre-alpha test
+
+Your RPNow server is ready!
+
+Data will be stored in ${config.dataDir}.
+Be sure to back this up regularly!
+
+${(process.platform === 'win32' || process.platform === 'darwin') ? (
+`To access RPNow on the local network, (i.e., when connected to the
+same wi-fi as this computer) try visiting one of the following in your
+web browser:`
+):(
+`To access this RPNow server, try visiting one of the following in your
+web browser:`
+)}
+
+${(myIpAddresses.length > 0) ? (
+myIpAddresses.map(ip => `* http://${ip}:${config.port}`)
+):(
+`  (Unable to determine my address.)`
+)}
+
+Have fun!
+`;
+
+module.exports = banner;
