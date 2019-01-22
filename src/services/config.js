5c23c7ee93ec2ed5c9eb22e59f1337bc90e76fb2
Nigel Nelson <nigel.mark.nelson@gmail.com>
2019-01-22 01:24:11 -0700

    Store rpnow data in home directory instead of application directory

--- src/services/config.js
+++ src/services/config.js
@@ -1,15 +1,14 @@
 const path = require('path');
+const os = require('os');
 
-function getProjectDirectory() {
-    if (process.pkg) {
-        return path.dirname(process.argv[0]);
-    } else {
-        return path.join(path.dirname(process.argv[1]), '..');
-    }
+function getDataDirectory() {
+    const userData = process.env.APPDATA || os.homedir()
+    
+    return path.join(userData, 'rpnow_data');
 }
 
 module.exports = {
     port: (+process.env.RPNOW_PORT) || 13000,
     trustProxy: (process.env.RPNOW_TRUST_PROXY || '').toLowerCase() === 'true',
-    dataDir: (process.env.RPNOW_DATA_DIR) || path.join(getProjectDirectory(), './data'),
+    dataDir: (process.env.RPNOW_DATA_DIR) || getDataDirectory(),
 };
