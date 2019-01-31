9c1f7e2d02aaf36e17fe40a5c7b0e11930aa9b6d
Nigel Nelson <nigel.mark.nelson@gmail.com>
2019-01-31 12:35:34 -0700

    Include INI file in zip distributable

--- build.sh
+++ build.sh
@@ -7,11 +7,12 @@ npx node-pre-gyp install --directory ./node_modules/sqlite3 --target_platform=wi
 
 mkdir -p ./dist/node_modules/sqlite3/lib
 cp -r ./node_modules/sqlite3/lib/binding ./dist/node_modules/sqlite3/lib/binding
+cp ./rpnow.ini ./dist
 
 npx pkg ./package.json --targets node8-win-x86 --output dist/RPNow.exe
 
 cd dist
-zip RPNow.zip RPNow.exe node_modules/sqlite3/lib/binding/node-v57-win32-ia32/node_sqlite3.node
+zip RPNow.zip RPNow.exe rpnow.ini node_modules/sqlite3/lib/binding/node-v57-win32-ia32/node_sqlite3.node
 
 rm -rf node_modules
 find . -type f ! -name '*.zip' -delete
