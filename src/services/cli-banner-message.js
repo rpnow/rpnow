cc5475dda3b2f0ff0d2d233c4a92313085002fbb
Nigel Nelson <nigel.mark.nelson@gmail.com>
2019-02-01 15:19:17 -0700

    Add info line about dataDir to the CLI

--- src/services/cli-banner-message.js
+++ src/services/cli-banner-message.js
@@ -5,14 +5,12 @@ const myIpAddresses = Object.values(os.networkInterfaces())
     .filter(x => x)
     .map(x => x.address);
 
-const banner = ({ port, ssl, sslPort, sslDomain }) => `
+const banner = ({ port, ssl, sslPort, sslDomain, dataDir }) => `
  _ __ _ __  _ __   _____      __
 | '__| '_ \\| '_ \\ / _ \\ \\ /\\ / /
 | |  | |_) | | | | (_) \\ V  V /
 |_|  | .__/|_| |_|\\___/ \\_/\\_/
      |_|         pre-alpha test
-
-Your RPNow server is ready!
 
 ${(sslDomain) ? (
 `To access this RPNow server, try visiting the following in your
@@ -37,6 +35,8 @@ myIpAddresses.map(ip => {
 ):(
 `  (Unable to determine my address.)`
 )}`)}
+
+RP data will be stored in ${dataDir}
 
 Have fun!
 `;
