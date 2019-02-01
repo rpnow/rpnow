ce6896aa4a7eb6380a9aa013fe9ce8fe24d0ae4c
Nigel Nelson <nigel.mark.nelson@gmail.com>
2019-02-01 15:51:13 -0700

    Comments describing the build.sh process

--- build.sh
+++ build.sh
@@ -1,21 +1,28 @@
 #!/bin/sh
 
+# Clean dist folder
 rm -rf dist
 mkdir dist
 
+# Get native sqlite3 bindings for all platforms, and copy them in
 npx node-pre-gyp install --directory ./node_modules/sqlite3 --target_platform=win32 --target_arch=ia32
 npx node-pre-gyp install --directory ./node_modules/sqlite3 --target_platform=linux --target_arch=x64
 
 mkdir -p ./dist/node_modules/sqlite3/lib
 cp -r ./node_modules/sqlite3/lib/binding ./dist/node_modules/sqlite3/lib/binding
+
+# Copy in sample rpnow.ini
 cp ./rpnow.ini ./dist
 
+# Build exe files
 npx pkg ./package.json --targets node8-win-x86 --output dist/RPNow.exe
 npx pkg ./package.json --targets node8-linux-x64 --output dist/rpnow
 
+# Zip up the distributables
 cd dist
 zip rpnow-windows.zip RPNow.exe rpnow.ini node_modules/sqlite3/lib/binding/node-v57-win32-ia32/node_sqlite3.node
 zip rpnow-linux.zip rpnow rpnow.ini node_modules/sqlite3/lib/binding/node-v57-linux-x64/node_sqlite3.node
 
+# Clean up dist folder
 rm -rf node_modules
 find . -type f ! -name '*.zip' -delete
