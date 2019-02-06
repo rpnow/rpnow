38e31ba191e83678f4cd79aad0437956b828eb5e
Nigel Nelson <nigel.mark.nelson@gmail.com>
2019-02-06 14:39:43 -0700

    server routes shape

--- server.go
+++ server.go
@@ -9,25 +9,52 @@ import (
 )
 
 func main() {
+	// create router
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", index).Methods("GET")
-	router.HandleFunc("/todos", todoIndex)
-	router.HandleFunc("/todos/{todoId}", todoShow)
-	router.PathPrefix("/").Handler(http.FileServer(http.Dir("src/static")))
 
+	// api
+	router.HandleFunc("/api/health", todo).Methods("GET")
+	router.HandleFunc("/api/rp", todo).Methods("POST")
+	router.HandleFunc("/api/rp/import", todo).Methods("POST")
+	router.HandleFunc("/api/rp/import/{id:[-0-9a-zA-Z]+}", todo).Methods("POST")
+	router.HandleFunc("/api/user", todo).Methods("POST")
+	router.HandleFunc("/api/user/verify", todo).Methods("GET")
+	router.HandleFunc("/api/rp/{id:[-0-9a-zA-Z]+}", todo).Methods("GET")
+	router.HandleFunc("/api/rp/{id:[-0-9a-zA-Z]+}/updates", todo).Methods("GET")
+	router.HandleFunc("/api/rp/{id:[-0-9a-zA-Z]+}/pages", todo).Methods("GET")
+	router.HandleFunc("/api/rp/{id:[-0-9a-zA-Z]+}/pages/{pageNum:[1-9][0-9]*}", todo).Methods("GET")
+	router.HandleFunc("/api/rp/{id:[-0-9a-zA-Z]+}/download.txt", todo).Methods("GET")
+	router.HandleFunc("/api/rp/{id:[-0-9a-zA-Z]+}/export", todo).Methods("GET")
+	router.HandleFunc("/api/rp/{id:[-0-9a-zA-Z]+}/{collections:[a-z]+}", todo).Methods("POST")
+	router.HandleFunc("/api/rp/{id:[-0-9a-zA-Z]+}/{collections:[a-z]+}/{docId:[0-9a-z]+}", todo).Methods("PUT")
+	router.HandleFunc("/api/rp/{id:[-0-9a-zA-Z]+}/{collections:[a-z]+}/history", todo).Methods("GET")
+	router.PathPrefix("/api").HandlerFunc(apiMalformed)
+
+	// routes
+	router.HandleFunc("/", indexHTML).Methods("GET")
+	router.HandleFunc("/terms", indexHTML).Methods("GET")
+	router.HandleFunc("/format", indexHTML).Methods("GET")
+	router.HandleFunc("/rp/{rpCode}", indexHTML).Methods("GET")
+	router.HandleFunc("/read/{rpCode}", indexHTML).Methods("GET")
+	router.HandleFunc("/read/{rpCode}/page/{page}", indexHTML).Methods("GET")
+
+	// assets
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("views/dist")))
+
+	// listen
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Welcome!")
+func indexHTML(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "views/dist/index.html")
 }
 
-func todoIndex(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Todo Index!")
+func apiMalformed(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusBadRequest)
+	fmt.Fprintln(w, "{\"error\":\"Malformed request\"}")
 }
 
-func todoShow(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	todoId := vars["todoId"]
-	fmt.Fprintln(w, "Todo show:", todoId)
+func todo(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+	fmt.Fprintln(w, "TODO")
 }
