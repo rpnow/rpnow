e32b94b2c30e3a9fce89ffc3130222cc1c99115e
Nigel Nelson <nigel.mark.nelson@gmail.com>
2019-02-15 18:34:27 -0700

    WIP posting data to db

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"regexp"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -16,7 +17,7 @@ import (
 	"github.com/rs/xid"
 )
 
-var port = 8080
+var port = 13000
 var addr = fmt.Sprintf(":%d", port)
 
 func main() {
@@ -53,9 +54,9 @@ func main() {
 	roomAPI.HandleFunc("/pages/{pageNum:[1-9][0-9]*}", todo).Methods("GET")
 	roomAPI.HandleFunc("/download.txt", todo).Methods("GET")
 	roomAPI.HandleFunc("/export", todo).Methods("GET")
-	roomAPI.HandleFunc("/{collections:[a-z]+}", todo).Methods("POST")
-	roomAPI.HandleFunc("/{collections:[a-z]+}/{docId:[0-9a-z]+}", todo).Methods("PUT")
-	roomAPI.HandleFunc("/{collections:[a-z]+}/history", todo).Methods("GET")
+	roomAPI.HandleFunc("/{collectionName:[a-z]+}", rpSendThing).Methods("POST")
+	roomAPI.HandleFunc("/{collectionName:[a-z]+}/{docId:[0-9a-z]+}", todo).Methods("PUT")
+	roomAPI.HandleFunc("/{collectionName:[a-z]+}/history", todo).Methods("GET")
 	api.PathPrefix("/").HandlerFunc(apiMalformed)
 
 	// routes
@@ -178,6 +179,99 @@ func rpChatUpdates(w http.ResponseWriter, r *http.Request) {
 		"lastEventId": params["since"],
 		"updates":     []interface{}{},
 	})
+}
+
+type RpMessageBody struct {
+	Type    string `json:"type"`
+	Content string `json:"content"`
+	URL     string `json:"url"`
+	CharaID string `json:"charaId,omitempty"`
+}
+
+func (m *RpMessageBody) Validate() error {
+	if m.Type == "image" {
+		if m.Content != "" {
+			return fmt.Errorf("Msg: image should not have 'content'")
+		}
+		if m.CharaID != "" {
+			return fmt.Errorf("Msg: image should not have 'charaId'")
+		}
+		urlRegexp := regexp.MustCompile("^https?://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+$")
+		if urlRegexp.MatchString(m.URL) {
+			return fmt.Errorf("Msg: url is invalid")
+		}
+		return nil
+	} else if m.Type == "narrator" || m.Type == "chara" || m.Type == "ooc" {
+		if m.URL != "" {
+			return fmt.Errorf("Msg: non-image should not have 'url'")
+		}
+		if m.Content == "" {
+			return fmt.Errorf("Msg: content is empty")
+		}
+		if len(m.Content) > 10000 {
+			return fmt.Errorf("Msg: content is too long (%d characters)", len(m.Content))
+		}
+		if m.Type == "chara" {
+			if m.CharaID == "" {
+				return fmt.Errorf("Msg: charaId is empty")
+			}
+			// TODO check if the doc is in the db
+		} else {
+			if m.CharaID != "" {
+				return fmt.Errorf("Msg: non-chara msg should not have 'charaId'")
+			}
+		}
+		return nil
+	} else {
+		return fmt.Errorf("Msg: invalid type")
+	}
+}
+
+type RpMessage struct {
+	*RpMessageBody
+}
+
+type RpCharaBody struct {
+	Name  string `json:"name"`
+	Color string `json:"color"`
+}
+
+type RpChara struct {
+	*RpCharaBody
+}
+
+func (c *RpCharaBody) Validate() error {
+	if len(c.Name) == 0 {
+		return fmt.Errorf("Chara: name is empty")
+	}
+	if len(c.Name) > 30 {
+		return fmt.Errorf("Chara: name is too long")
+	}
+	colorRegexp := regexp.MustCompile("^#[0-9a-f]{6}$")
+	if !colorRegexp.MatchString(c.Color) {
+		return fmt.Errorf("Chara: color is invalid")
+	}
+	return nil
+}
+
+func rpSendThing(w http.ResponseWriter, r *http.Request) {
+	// generate key for new object
+	params := mux.Vars(r)
+	var slugInfo SlugInfo
+	err := db.One("slug_"+params["slug"], &slugInfo)
+	if err != nil {
+		panic(err)
+	}
+	id := xid.New().String()
+	key := slugInfo.Rpid + "_" + params["collectionName"] + "_" + id
+
+	// validate value
+	// TODO
+
+	// put it in the db
+	db.Add(key, []byte{})
+
+	// bounce it back and send
 }
 
 func createUser(w http.ResponseWriter, r *http.Request) {
