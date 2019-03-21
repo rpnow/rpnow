18f3dabc7f81e1a035d50604c3f0db2f1d9845ff
Nigel Nelson <nigel.mark.nelson@gmail.com>
2019-03-21 16:04:52 -0600

    WIP shutting down message; needs new link

--- client/src/app/rp/components/banner-message.component.ts
+++ client/src/app/rp/components/banner-message.component.ts
@@ -3,29 +3,42 @@ import { Component, ChangeDetectionStrategy, Input, EventEmitter, Output } from 
 @Component({
   selector: 'rpn-banner-message',
   template: `
-    <div *ngIf="showTos" id="tos">
+    <div *ngIf="showShutdown" id="shutdown">
 
       <span class="generated-links-contrast">
-        By using RPNow, you agree to its <a class="link" target="_blank" href="/terms">terms of use.</a>
+        <strong>Rpnow.net will permanently shut down on June 30.</strong> <br/><a class="link" target="_blank" href="https://blog.rpnow.net/post/179967000616/whats-happening-to-rpnownet">More info here.</a>
       </span>
 
-      <button mat-icon-button (click)="acceptTerms.emit()">
-        <mat-icon>close</mat-icon>
-      </button>
-
-    </div>
-    <div *ngIf="hasUpdate" id="update">
-
-      <span class="generated-links-contrast">
-        An update is available! <a href="javascript:;" (click)="activateUpdate.emit()">Update now</a> to use the latest features.
-      </span>
-
-      <button mat-icon-button (click)="hasUpdate = false">
+      <button mat-icon-button (click)="showShutdown = false">
         <mat-icon>close</mat-icon>
       </button>
 
     </div>
   `,
+  // template: `
+  //   <div *ngIf="showTos" id="tos">
+
+  //     <span class="generated-links-contrast">
+  //       By using RPNow, you agree to its <a class="link" target="_blank" href="/terms">terms of use.</a>
+  //     </span>
+
+  //     <button mat-icon-button (click)="acceptTerms.emit()">
+  //       <mat-icon>close</mat-icon>
+  //     </button>
+
+  //   </div>
+  //   <div *ngIf="hasUpdate" id="update">
+
+  //     <span class="generated-links-contrast">
+  //       An update is available! <a href="javascript:;" (click)="activateUpdate.emit()">Update now</a> to use the latest features.
+  //     </span>
+
+  //     <button mat-icon-button (click)="hasUpdate = false">
+  //       <mat-icon>close</mat-icon>
+  //     </button>
+
+  //   </div>
+  // `,
   styles: [`
     /* TODO figure out how to import this in an inline styles declaration */
     /* @import '~@angular/material/theming'; */
@@ -56,10 +69,16 @@ import { Component, ChangeDetectionStrategy, Input, EventEmitter, Output } from 
       color: black;
       background-color: #2196F3;
     }
+    div#shutdown {
+      color: white;
+      background-color: #C62828;
+    }
   `],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BannerMessageComponent {
+
+  showShutdown = true;
 
   @Input() showTos: boolean;
   @Input() hasUpdate: boolean;
