e73a44f81bd995842a467dd2258873f0ea456318
Nigel Nelson <nigel.mark.nelson@gmail.com>
2019-04-02 23:04:56 -0600

    WIP dashboard improvements

--- views/src/pages/home.vue
+++ views/src/pages/home.vue
@@ -1,20 +1,25 @@
 <template>
   <div id="homepage">
-    <h1>{{ siteName }}</h1>
-
     <div v-if="!loading">
-      <div v-if="canCreate">
-        <button @click="createRp">Create New RP</button>
+      <h1>My RPs</h1>
+      <div id="top-options">
+        <button class="pane" id="new-rp" v-if="canCreate" @click="createRp">
+          Create New RP
+        </button>
+        <a class="pane" id="import-rp" v-if="canImport" href="/import">
+          Import RP
+        </a>
       </div>
-      <div v-if="canImport">
-        <a href="/import">Import RP</a>
-      </div>
-      <div v-if="recentRooms.length > 0">
-        <h2>My recent RPs</h2>
-      </div>
-      <div v-for="room of recentRooms" :key="'room'+room.rpCode">
-        <a :href="'/rp/'+room.rpCode">{{ room.title }}</a>
-      </div>
+      <template v-for="room of recentRooms">
+        <a :key="'room'+room.rpCode" class="pane recent-rp" :href="'/rp/'+room.rpCode">
+          <div class="pretty-block">
+            <span class="rp-date">3/27/09</span>
+          </div>
+          <div class="rp-title">
+            {{ room.title }}
+          </div>
+        </a>
+      </template>
     </div>
 
     <div id="loading" v-else>
@@ -25,6 +30,68 @@
   </div>
 </template>
 
+<style scoped>
+  #homepage {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 0 5%;
+    box-sizing: border-box;
+  }
+  h1 {
+    margin-top: 30px;
+    text-align: center;
+    font-weight: normal;
+  }
+  #top-options {
+    display: flex;
+    width: 300px;
+  }
+  .pane {
+    display: block;
+    height: 100px;
+    border: solid 1px rgba(0,0,0,0.5);
+    background: none;
+    font-family: inherit;
+    cursor: pointer;
+    margin: 0 5px 10px;
+    text-decoration: none;
+    color: inherit;
+    box-sizing: border-box;
+    border-radius: 10px;
+    overflow: hidden;
+  }
+  .recent-rp {
+    display: flex;
+    flex-direction: column;
+  }
+  .pretty-block {
+    flex: 1;
+    position: relative;
+    background-color: aquamarine;
+  }
+  .rp-date {
+    position: absolute;
+    right: 4px;
+    top: 2px;
+  }
+  .rp-title {
+    padding: 3px 10px;
+  }
+  #new-rp {
+    height: 60px;
+    font-size: inherit;
+    flex-grow: 3;
+  }
+  #import-rp {
+    height: 60px;
+    flex-grow: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
+
 <script>
   import axios from 'axios';
   import initializeAuth from '../components/user';
@@ -33,16 +100,14 @@
   export default {
     data: function() {
       return {
-        siteName: 'rpnow',
         recentRooms: [],
-        title: '',
         loading: true,
         canCreate: false,
         canImport: false,
       };
     },
     beforeMount: function() {
-      this.siteName = location.hostname;
+      document.title = 'My RPs on ' + location.hostname;
       try {
         this.recentRooms = JSON.parse(localStorage.getItem('rpnow.global.recentRooms') || '[]')
       } catch (err) {
@@ -95,21 +160,3 @@
   };
 </script>
 
-<style scoped>
-  #homepage {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin: 0 5%;
-    box-sizing: border-box;
-  }
-  h1 {
-    padding: 10vh 0 1.7vh;
-    margin: -2vmin 0 0;
-    text-align: center;
-    font-size: calc(12px + 7vmin);
-    line-height: 1;
-    font-family: "Playfair Display";
-    font-weight: normal;
-  }
-</style>
