3f0bd37ff1b2f0bb550a4e748834fb9a3824b23d
Nigel Nelson <nigel.mark.nelson@gmail.com>
2019-04-05 10:52:58 -0600

    Make SlugInfo persistent

--- server/db.go
+++ server/db.go
@@ -2,31 +2,78 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 
 	"github.com/boltdb/bolt"
 )
 
 var rpsByID map[string]*RP
-var slugMap map[string]SlugInfo
 var revisions map[string][]json.RawMessage
 
 func init() {
 	rpsByID = make(map[string]*RP)
-	slugMap = make(map[string]SlugInfo)
 	revisions = make(map[string][]json.RawMessage)
 }
 
-var db struct {
-	bdb *bolt.DB
+type database struct {
+	bolt *bolt.DB
 }
 
+var db *database
+
+// ROOMS: rpid -> json
+// MSGS: rpid/xid -> json
+// CHARAS: rpid/xid -> json
+// SLUGS: slug -> {rpid,access}
+// REVISIONS: (msg|chara)id -> []json
+
 func init() {
+	// path to boltdb file
 	path := "../data/rpnow.boltdb"
 
-	bdb, err := bolt.Open(path, 0600, nil)
+	// initialize boltdb
+	boltdb, err := bolt.Open(path, 0600, nil)
 	if err != nil {
 		log.Fatalf("Could not open bolt db at %s", path)
 	}
-	db.bdb = bdb
+
+	// initialize buckets
+	err = boltdb.Update(func(tx *bolt.Tx) (err error) {
+		for _, bucketName := range []string{"rooms", "slugs", "msgs", "charas", "revisions"} {
+			_, err = tx.CreateBucketIfNotExists([]byte(bucketName))
+			if err != nil {
+				return err
+			}
+		}
+		return
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// now we can use it
+	db = &database{boltdb}
+}
+
+func (db *database) getSlugInfo(rpid string, out interface{}) error {
+	return db.bolt.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte("slugs"))
+		js := bucket.Get([]byte(rpid))
+		if js == nil {
+			return fmt.Errorf("Could not find slug @ %s", rpid)
+		}
+		return json.Unmarshal(js, out)
+	})
+}
+
+func (db *database) addSlugInfo(rpid string, value interface{}) error {
+	return db.bolt.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte("slugs"))
+		js, err := json.Marshal(value)
+		if err != nil {
+			return err
+		}
+		return bucket.Put([]byte(rpid), js)
+	})
 }
