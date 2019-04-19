4b394d025ac0d29a610ae5b239ee12ad8bea0339
Nigel Nelson <nigel.mark.nelson@gmail.com>
2019-04-19 00:12:01 -0600

    select config file from cmd line args

--- server/main.go
+++ server/main.go
@@ -3,25 +3,30 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"os"
 	"os/signal"
 )
 
 func main() {
-	// Print "Goodbye" after all defer statements are done
-	defer log.Println("Goodbye!")
+	// get path to config file from cmd args
+	configPath := flag.String("config", "/etc/rpnow.ini", "path to the server config file")
+	flag.Parse()
 
 	// load config
 	conf := defaultServerConf()
-	conf.loadFromINI("/etc/rpnow.ini")
+	conf.loadFromINI(*configPath)
 	conf.dataDir = "../data"
 	conf.port = 13000
 
 	// run server
 	server := &Server{conf: conf}
 	stop := server.run()
-	defer stop()
+	defer func() {
+		stop()
+		log.Println("Goodbye!")
+	}()
 
 	// await kill signal
 	c := make(chan os.Signal, 1)
