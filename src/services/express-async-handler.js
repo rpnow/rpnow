af833f8f9c8bcff35485b493333f65c08e4ee7fa
Nigel Nelson <nigel.mark.nelson@gmail.com>
2019-05-13 18:55:00 -0600

    Remove node-based server

--- src/services/express-async-handler.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    awrap(fn) {
-        return function asyncWrapper(...args) {
-            const next = args[args.length - 1];
-            return fn(...args).catch(next);
-        }
-    }
-};
